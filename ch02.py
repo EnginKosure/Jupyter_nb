0549800d41d2b470c0140390b9afb99e1d19c70e
EnginKosure <engin.kosure@cronos.be>
2020-07-30 23:24:13 +0200

    consecutive sequence

---
+++ ch02.py
@@ -0,0 +1,17 @@
+# Write a function that returns True if two lists, when combined, form a consecutive sequence.
+# consecutive_combo([7, 4, 5, 1], [2, 3, 6]) ➞ True
+
+# consecutive_combo([1, 4, 6, 5], [2, 7, 8, 9]) ➞ False
+
+# consecutive_combo([1, 4, 5, 6], [2, 3, 7, 8, 10]) ➞ False
+
+# consecutive_combo([44, 46], [45]) ➞ True
+
+
+def consecutive_combo(lst1, lst2):
+    n_l = lst1+lst2
+    return [i for i in n_l if len(n_l) == n_l[-1]-n_l[0]-1]
+
+
+print(consecutive_combo([7, 4, 5, 1], [2, 3, 6]))
+print(consecutive_combo([1, 4, 5, 6], [2, 3, 7, 8, 10]))
