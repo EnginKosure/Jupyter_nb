b2043ab39fbf8aa6aba4d972bda319fcdd69c906
EnginKosure <engin.kosure@cronos.be>
2020-08-01 08:05:36 +0200

    football challenge solved

--- ch06.py
+++ ch06.py
@@ -8,10 +8,6 @@
 def calculate_PT(st):
     PT = {}
     x = st.split(' ')  # ['A', '0', '-', '1', 'B']
-    a, _, b, _, _, _, c, _, d = st
-    PAY = {}
-    PAY[a] = b
-    PAY[d] = c
     if x[1] > x[3]:
         PT[x[0]] = 3
         PT[x[4]] = 0
@@ -22,19 +18,31 @@ def calculate_PT(st):
     else:
         PT[x[4]] = 3
         PT[x[0]] = 0
-    print('PAY', PAY)
     return PT
+
+
+def calculate_PAY(st):
+    a, _, b, _, _, _, c, _, d = st
+    PAY = {}
+    PAY[a] = b
+    PAY[d] = c
+    return PAY
 
 
 # calculate_PT("A 0 - 1 B")
 y = ["A 0 - 1 B", "C 2 - 0 D", "B 2 - 2 C",
      "D 3 - 1 A", "A 2 - 2 C", "B 2 - 0 D"]
 calc_Total = list(map(calculate_PT, y))
+PAY_Total = list(map(calculate_PAY, y))
 print(calc_Total)
+print(PAY_Total)
 # [{'B': 3, 'A': 0}, {'C': 3, 'D': 0}, {'B': 1, 'C': 1}, {'D': 3, 'A': 0}, {'A': 1, 'C': 1}, {'B': 3, 'D': 0}]
 cc = [{k: v} for k, v in [(k, v) for x in calc_Total for (k, v) in x.items()]]
 # cc [{'B': 3}, {'A': 0}, {'C': 3}, {'D': 0}, {'B': 1}, {'C': 1}, {'D': 3}, {'A': 0}, {'A': 1}, {'C': 1}, {'B': 3}, {'D': 0}]
 print('cc', cc)
+pay_pay = [{k: int(v)}
+           for k, v in [(k, v) for x in PAY_Total for (k, v) in x.items()]]
+print('pay_pay', pay_pay)
 
 # print('ff', ff)
 dd = {}
@@ -46,3 +54,13 @@ for i in range(len(cc)):
         dd[tuple(cc[i].keys())] += list(cc[i].values())[0]
 
 print('dd', dd)
+
+gg = {}
+for i in range(len(pay_pay)):
+    print(pay_pay[i])
+    if tuple(pay_pay[i].keys()) not in gg:
+        gg[tuple(pay_pay[i].keys())] = list(pay_pay[i].values())[0]
+    else:
+        gg[tuple(pay_pay[i].keys())] += list(pay_pay[i].values())[0]
+
+print('gg', gg)
