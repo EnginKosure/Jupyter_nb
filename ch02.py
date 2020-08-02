8cb76a9487584c16de6cad50f3d0251c30737570
EnginKosure <engin.kosure@cronos.be>
2020-08-02 23:33:33 +0200

    comments deleted

--- ch02.py
+++ ch02.py
@@ -1,11 +1,4 @@
 # Write a function that returns True if two lists, when combined, form a consecutive sequence.
-# consecutive_combo([7, 4, 5, 1], [2, 3, 6]) ➞ True
-
-# consecutive_combo([1, 4, 6, 5], [2, 7, 8, 9]) ➞ False
-
-# consecutive_combo([1, 4, 5, 6], [2, 3, 7, 8, 10]) ➞ False
-
-# consecutive_combo([44, 46], [45]) ➞ True
 
 
 def consecutive_combo(lst1, lst2):
