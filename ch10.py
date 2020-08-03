6ca4d367f2005e1242483baf8769e31bb37edd1f
EnginKosure <engin.kosure@cronos.be>
2020-08-04 01:06:57 +0200

    mentoring question prepared

--- ch10.py
+++ ch10.py
@@ -1,8 +1,10 @@
 # Create a function that converts a date formatted as MM/DD/YYYY to YYYYDDMM.
+def format_date(x):
+    return ''.join(x.split('/')[::-1])
 
-# Examples
-# format_date("11/12/2019") ➞ "20191211"
 
-# format_date("12/31/2019") ➞ "20193112"
+print(format_date("11/12/2019"))  # ➞ "20191211"
 
-# format_date("01/15/2019") ➞ "20191501"
+format_date("12/31/2019")  # ➞ "20193112"
+
+format_date("01/15/2019")  # ➞ "20191501"
