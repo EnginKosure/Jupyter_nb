49c03a717622af2e55e98e4735e507549b76f388
EnginKosure <engin.kosure@cronos.be>
2020-08-05 20:27:23 +0200

    tictactoe new solution

--- ch11.py
+++ ch11.py
@@ -1,19 +1,34 @@
-def tic_tac_toe(lst):
-    if lst[0][0] == lst[1][0] == lst[2][0]:
-        return lst[0][0]
-    elif lst[0][1] == lst[1][1] == lst[2][1]:
-        return lst[0][1]
-    elif lst[0][2] == lst[1][2] == lst[2][2]:
-        return lst[0][2]
-    elif lst[0][0] == lst[1][1] == lst[2][2]:
-        return lst[0][0]
-    elif lst[0][2] == lst[1][1] == lst[2][0]:
-        return lst[0][2]
-    for i in lst:
-        if i[0] == i[1] == i[2]:
+# def tic_tac_toe(lst):
+#     if lst[0][0] == lst[1][0] == lst[2][0]:
+#         return lst[0][0]
+#     elif lst[0][1] == lst[1][1] == lst[2][1]:
+#         return lst[0][1]
+#     elif lst[0][2] == lst[1][2] == lst[2][2]:
+#         return lst[0][2]
+#     elif lst[0][0] == lst[1][1] == lst[2][2]:
+#         return lst[0][0]
+#     elif lst[0][2] == lst[1][1] == lst[2][0]:
+#         return lst[0][2]
+#     for i in lst:
+#         if i[0] == i[1] == i[2]:
+#             return i[0]
+#     else:
+#         return 'Draw'
+
+
+def tic_tac_toe(board):
+    for i in board:
+        if len(set(i)) == 1:
             return i[0]
-    else:
-        return 'Draw'
+    for i in list(zip(*board)):
+        print('ZIP', i)
+        if len(set(i)) == 1:
+            return i[0]
+    if len({board[i][i] for i in range(3)}) == 1:
+        return board[0][0]
+    if len({board[i][2-i] for i in range(3)}) == 1:
+        return board[0][2]
+    return 'Draw'
 
 
 print(tic_tac_toe([
