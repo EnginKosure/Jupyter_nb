fd6e0a9dd8bd627527f947f86caa25d3fcf340eb
EnginKosure <engin.kosure@cronos.be>
2020-08-06 23:38:00 +0200

    class solution added

--- ch11.py
+++ ch11.py
@@ -35,12 +35,16 @@ def tic_tac_toe(board):
     d1, d2 = [], []
     for i in range(3):
         col = [board[0][i], board[1][i], board[2][i]]
-        if len(set(board[i])) == 1:
-            return board[i][0]
+        if board[i].count('X') == 3 or col.count('X') == 3:
+            return 'X'
+        if board[i].count('O') == 3 or col.count('O') == 3:
+            return 'O'
         d1 += [board[i][i]]
         d2 += [board[i][2-i]]
-    if len(set(d1)) == 1:
-        return d1[0]
+    if len(set(d2)) == 1:
+        return 'X'
+    if d1.count('O') == 3:
+        return 'O'
     return 'Draw'
 
 
--- ch12.py
+++ ch12.py
@@ -3,3 +3,21 @@
 # items (default: []): A list of contents to paginate.
 
 # pageSize (default: 10): The amount of items to show in each page.
+
+
+class Pagination:
+
+    def __init__(self, items=[], pageSize=10):
+        self.items = items
+        self.pageSize = pageSize
+        self.totalPages = 0
+        self.currentPage = 0
+
+    def getItems(self):
+
+    def getPageSize(self):
+
+    def getCurrentPage(self):
+
+        # Goes to the previous page
+    def prevPage():
