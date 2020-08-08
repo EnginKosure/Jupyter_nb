2e66290cd8e00ce0c28c8f8f1b079bbff5e87183
EnginKosure <engin.kosure@cronos.be>
2020-08-08 23:37:03 +0200

    strings revisited

--- ch14.py
+++ ch14.py
@@ -3,3 +3,5 @@ x = string.ascii_lowercase
 print(x)  # 'abcdefghijklmnopqrstuvwxyz'
 y = list(map(chr, range(97, 123)))
 print(y)  # ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+z = list(map(chr, range(ord('a'), ord('z')+1)))
+print(z)
