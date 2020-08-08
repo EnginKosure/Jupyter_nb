1ba9fb424e39b89706371dd15d436a6d758d9eee
EnginKosure <engin.kosure@cronos.be>
2020-08-08 23:36:02 +0200

    strins revisited

--- ch14.py
+++ ch14.py
@@ -1,3 +1,5 @@
 import string
 x = string.ascii_lowercase
 print(x)  # 'abcdefghijklmnopqrstuvwxyz'
+y = list(map(chr, range(97, 123)))
+print(y)  # ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
