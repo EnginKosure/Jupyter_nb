b58bd1f30474205fe0d0e0338adb273b97f6032e
EnginKosure <engin.kosure@cronos.be>
2020-08-13 19:01:00 +0200

    new class created

--- ch21.py
+++ ch21.py
@@ -1,11 +1,49 @@
+class Pizza:
+    order = 0
+
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+        Pizza.order += 1
+        self.order_number = Pizza.order
+
+    hawaiian = classmethod(lambda cls: cls(["ham", "pineapple"]))
+
+    @classmethod
+    def meat_festival(cls):
+        return cls('beef, meatball, bacon'.split(', '))
+
+    @staticmethod
+    def garden_feast():
+        return Pizza(['spinach', 'olives', 'mushroom'])
+
+    '''
+	@property
+	def hawaiian(self):
+		return self.ingredients
+	@property
+	def meat_festival(self):
+		return self.ingredients
+	@property
+	def garden_feast(self):
+		return self.ingredients
+	@hawaiian.setter
+	def hawaiian(self):
+		self.ingredients = ['ham', 'pineapple']
+	@meat_festival.setter
+	def meat_festival(self):
+		self.ingredients = ['beef', 'meatball', 'bacon']
+	@garden_feast.setter
+	def garden_feast(self):
+		self.ingredients = ['spinach', 'olives', 'mushroom']
+'''
 
 
 p1 = Pizza(["bacon", "parmesan", "ham"])    # order 1
 p2 = Pizza.garden_feast()                  # order 2
 p1.ingredients  # ➞ ["bacon", "parmesan", "ham"]
 
-p2.ingredients  # ➞ ["spinach", "olives", "mushroom"]
+print(p2.ingredients)  # ➞ ["spinach", "olives", "mushroom"]
 
-p1.order_number  # ➞ 1
+print(p1.order_number)  # ➞ 1
 
-p2.order_number  # ➞ 2
+print(p2.order_number)  # ➞ 2
