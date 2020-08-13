e2512d17ace483debaca5af33fa8bc562182167f
EnginKosure <engin.kosure@cronos.be>
2020-08-13 17:54:25 +0200

    new class created

---
+++ ch21.py
@@ -0,0 +1,11 @@
+
+
+p1 = Pizza(["bacon", "parmesan", "ham"])    # order 1
+p2 = Pizza.garden_feast()                  # order 2
+p1.ingredients  # ➞ ["bacon", "parmesan", "ham"]
+
+p2.ingredients  # ➞ ["spinach", "olives", "mushroom"]
+
+p1.order_number  # ➞ 1
+
+p2.order_number  # ➞ 2
