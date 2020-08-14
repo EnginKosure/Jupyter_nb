762c86e58b69be4caf5d7fd71096bbff97aa7b10
EnginKosure <engin.kosure@cronos.be>
2020-08-15 01:19:56 +0200

    different approaches added

--- ch23.py
+++ ch23.py
@@ -1,9 +1,20 @@
+# https://edabit.com/challenge/6brSyFwWnb9Msu7kX
+
+
 def pos_neg_sort(lst):
     a = sorted([x for x in lst if x > 0])
     for i in range(len(lst)):
         if lst[i] < 0:
             a.insert(i, lst[i])
     return a
+
+# def pos_neg_sort(lst):
+#   pos = sorted([i for i in lst if i>0],reverse=True)
+#   return [pos.pop() if j>0 else j for j in lst]
+
+# def pos_neg_sort(lst):
+# 	positives = iter(sorted(i for i in lst if i > 0))
+# 	return [next(positives) if i > 0 else i for i in lst]
 
 
 print(pos_neg_sort([6, 3, -2, 5, -8, 2, -2]))  # ➞ [2, 3, -2, 5, -8, 6, -2]
