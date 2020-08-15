6358c446b67fab460afc43cf21c10307b4c03138
EnginKosure <engin.kosure@cronos.be>
2020-08-16 00:24:52 +0200

    sort applied

--- ch24.py
+++ ch24.py
@@ -1,11 +1,14 @@
 def last_name_lensort(lst):
+    return sorted(lst, key=lambda i: (len(i.split(' ')[1]), i.split(' ')[1]))
+
+#sorted(inputlist, key=lambda e: (len(e[0]), e[0], e[1]))
 
 
-last_name_lensort([
+print(last_name_lensort([
     "Jennifer Figueroa",
     "Heather Mcgee",
     "Amanda Schwartz",
     "Nicole Yoder",
     "Melissa Hoffman"
-])
+]))
 # ➞ ["Heather Mcgee", "Nicole Yoder", "Melissa Hoffman", "Jennifer Figueroa", "Amanda Schwartz"]
