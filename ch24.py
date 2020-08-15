27ba25cf41821bc620deb23e36101103cee7f42e
EnginKosure <engin.kosure@cronos.be>
2020-08-16 00:02:20 +0200

    sort applied

---
+++ ch24.py
@@ -0,0 +1,11 @@
+def last_name_lensort(lst):
+
+
+last_name_lensort([
+    "Jennifer Figueroa",
+    "Heather Mcgee",
+    "Amanda Schwartz",
+    "Nicole Yoder",
+    "Melissa Hoffman"
+])
+# ➞ ["Heather Mcgee", "Nicole Yoder", "Melissa Hoffman", "Jennifer Figueroa", "Amanda Schwartz"]
