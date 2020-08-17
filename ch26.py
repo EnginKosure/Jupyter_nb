e3aed1d51429f3d4c1416656e15e8cc30a7056c2
EnginKosure <engin.kosure@cronos.be>
2020-08-17 08:07:43 +0200

    color validator added

--- ch26.py
+++ ch26.py
@@ -1,11 +1,19 @@
+import re
+
+
 def valid_color(s):
-    return s[:3] == 'rgb' or s[:4] == 'rgba' and eval(0 <= s[5] <= 255)
+    st = s  # Your Hex
+
+    match = re.search(
+        r'rgba?\(((25[0-5]|2[0-4]\d|1\d{1,2}|\d\d?)\s*[ \t]?,[ \t]?\s*?){2}(25[0-5]|2[0-4]\d|1\d{1,2}|\d\d?)\s*,?\s*([01]\.?\d*?)?\)', st)
+# \bc[ \t]*a[ \t]*t[ \t]*s\b
+    return bool(match)
 
 
 print(valid_color("rgb(0,0,0)"))  # ➞ True
 
-valid_color("rgb(0,,0)")  # ➞ False
+# print(valid_color("rgb(0,,0)"))  # ➞ False
 
-valid_color("rgb(255,256,255)")  # ➞ False
+print(valid_color("rgb(255,256,255)"))  # ➞ False
 
-valid_color("rgba(0,0,0,0.123456789)")  # ➞ True
+print(valid_color("rgba(0,0,0,0.123456789)"))  # ➞ True
