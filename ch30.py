22de36640ece2b0f0fc399240a5e9c55b7bdcd40
EnginKosure <engin.kosure@cronos.be>
2020-08-23 23:51:51 +0200

    python data type identification solved

--- ch30.py
+++ ch30.py
@@ -3,7 +3,27 @@
 # Finally return the total in a list.
 
 
-# [int, str, bool, list, tuple, dictionary]
+# [int, str, bool, list, tuple, dict]
+
+def count_datatypes(*args):
+    my_list = [0, 0, 0, 0, 0, 0]
+    for i in args:
+        if type(i) == int:
+            my_list[0] += 1
+        elif type(i) == str:
+            my_list[1] += 1
+        elif type(i) == bool:
+            my_list[2] += 1
+        elif type(i) == list:
+            my_list[3] += 1
+        elif type(i) == tuple:
+            my_list[4] += 1
+        elif type(i) == dict:
+            my_list[5] += 1
+    print(my_list)
+    return my_list
+
+
 count_datatypes(1, 45, "Hi", False)  # ➞ [2, 1, 1, 0, 0, 0]
 
 count_datatypes([10, 20], ("t", "Ok"), 2, 3, 1)  # ➞ [3, 0, 0, 1, 1, 0]
