bb3c664b0ef496d41c09c7b3e0a2fd90e37628e8
EnginKosure <engin.kosure@cronos.be>
2020-08-26 23:56:44 +0200

    daily challenge started

--- ch32.py
+++ ch32.py
@@ -1,20 +1,17 @@
 # Create a function that takes a string txt and censors any word from a given list
 # The text removed must be replaced by the given character char.
 
-import re
 
-
-def censor_string(s, l, r):
-    # for i in s.split():
-    #     if i in l:
-    #         s = s.replace(i, r*len(i))
-    # print(s)
-    # return s
-    for i in l:
-        if ' '+i.lower() in s:
-            s = re.sub(i.lower(), r*len(i), s)
-    print(s)
-    return s
+def censor_string(txt, lst, char):
+    res = ""
+    aux = txt.split(" ")
+    for x in aux:
+        if x in lst:
+            res = res + " " + char * len(x)
+        else:
+            res = res + " " + x
+    print(res[1:])
+    return res[1:]
 
 
 censor_string("Today is a Wednesday!", ["Today", "a"], "-")
