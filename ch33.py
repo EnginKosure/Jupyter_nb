9d04febc963948b40f99e51bb0d2e588abeb2922
EnginKosure <engin.kosure@cronos.be>
2020-08-28 23:55:38 +0200

    daily challenge started

---
+++ ch33.py
@@ -0,0 +1,9 @@
+# Create a function that returns the majority vote in a list.
+# A majority vote is an element that occurs > N/2 times in a list
+# (where N is the length of the list).
+
+majority_vote(["A", "A", "B"])  # ➞ "A"
+
+majority_vote(["A", "A", "A", "B", "C", "A"])  # ➞ "A"
+
+majority_vote(["A", "B", "B", "A", "C", "C"])  # ➞ None
