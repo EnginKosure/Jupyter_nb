db5daba310db8b8ef30f29393cdab87726c90952
EnginKosure <engin.kosure@cronos.be>
2020-08-29 08:09:02 +0200

    solution updated

--- ch33.py
+++ ch33.py
@@ -3,17 +3,19 @@
 # (where N is the length of the list).
 
 
-def majority_vote(l):
-    a = sorted([l.count(i) for i in l], reverse=True)
-    a = set(a)
-    print(a)
-    if len(a) == 1 or len(a) == 0:
-        return None
-    return sorted(l, key=l.count, reverse=True)[0]
+# def majority_vote(l):
+#     a = sorted([l.count(i) for i in l], reverse=True)
+#     a = set(a)
+#     if (len(a) == 1 and l[1:] != l[:-1]) or len(a) == 0:
+#         return None
+#     return sorted(l, key=l.count, reverse=True)[0]
+
+def majority_vote(lst):
+    return list(set([i if lst.count(i) > len(lst)//2 else None for i in set(lst)]))[0]
 
 
 print(majority_vote(["A", "A", "B"]))  # ➞ "A"
 
 print(majority_vote(["A", "A", "A", "B", "C", "A"]))  # ➞ "A"
 
-print(majority_vote(["A", "B", "B", "A", "C", "C"]))  # ➞ None
+print(majority_vote(["A", "B", "B", "A", "C", "C", 'C']))  # ➞ None
