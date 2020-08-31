958c3961b99e3eb99d5eb4a0f601f42a718f3152
EnginKosure <engin.kosure@cronos.be>
2020-08-31 22:50:31 +0200

    pentagonal number calculated

--- ch35.py
+++ ch35.py
@@ -1,10 +1,17 @@
 # Write a function that takes a positive integer and calculates how many dots exist in a pentagonal shape around the center dot on the Nth iteration.
 
 
-pentagonal(1)  # ➞ 1
+def pentagonal(n):
+    if n == 1:
+        return 1
+    else:
+        return 5*(n-1)+pentagonal(n-1)
 
-pentagonal(2)  # ➞ 6
 
-pentagonal(3)  # ➞ 16
+print(pentagonal(1))  # ➞ 1
 
-pentagonal(8)  # ➞ 141
+print(pentagonal(2))  # ➞ 6
+
+print(pentagonal(3))  # ➞ 16
+
+print(pentagonal(8))  # ➞ 141
