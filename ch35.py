efe8dcc33b82c2baf47efd0118deffd8263eab94
EnginKosure <engin.kosure@cronos.be>
2020-08-31 22:47:21 +0200

    pentagonal number calculated

---
+++ ch35.py
@@ -0,0 +1,10 @@
+# Write a function that takes a positive integer and calculates how many dots exist in a pentagonal shape around the center dot on the Nth iteration.
+
+
+pentagonal(1)  # ➞ 1
+
+pentagonal(2)  # ➞ 6
+
+pentagonal(3)  # ➞ 16
+
+pentagonal(8)  # ➞ 141
