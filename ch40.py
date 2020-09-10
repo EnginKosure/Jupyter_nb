0905bd4a3772495d4e9923bcf419d0fe1e61f793
EnginKosure <engin.kosure@cronos.be>
2020-09-10 23:17:28 +0200

    daily python challenge solved

--- ch40.py
+++ ch40.py
@@ -7,16 +7,20 @@
 # the longest substring with k distinct characters is "bcb".
 
 
+# def longest(s, k):
+#     long = ''
+#     for i in range(len(s)):
+#         for j in range(i + 1, len(s) + 1):
+#             sub = s[i:j]
+#             if len(set(sub)) > k:
+#                 break
+#             if len(sub) > len(long):
+#                 long = sub
+#     return long
+
 def longest(s, k):
-    long = ''
-    for i in range(len(s)):
-        for j in range(i + 1, len(s) + 1):
-            sub = s[i:j]
-            if len(set(sub)) > k:
-                break
-            if len(sub) > len(long):
-                long = sub
-    return long
+    return sorted([s[i:j] for i in range(len(s)) for j in range(
+        i + 1, len(s) + 1) if len(set(list(s[i:j]))) <= k], key=len)[-1]
 
 
 s = "abcba"
